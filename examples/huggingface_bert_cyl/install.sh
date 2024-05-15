80c8fbf8741afd73d0063bc5a692994d3d8fc7d8
15927303165 <690963696@qq.com>
2024-05-15 15:23:28 +0800

    update

---
+++ examples/huggingface_bert_cyl/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -xe
+
+# 0.4.14 raises error
+pip install --upgrade "jax[cuda12_pip]!=0.4.14" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# torch is required for checkpoint loading
+pip install transformers flax torch
