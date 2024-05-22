643dfc1a9a059be68f2e092f5fb2590a0d2e74b4
15927303165 <690963696@qq.com>
2024-05-22 10:00:10 +0800

    update

--- examples/huggingface_bert_cyl/server_embed.py
+++ examples/huggingface_bert_cyl/server_embed.py
@@ -42,7 +42,7 @@ class _InferFuncWrapper:
         self._device = device
         self._tokenizer = tokenizer
 
-    # 返回的embedding转换成bytes
+    # 使用TRT
     @batch
     @group_by_values("max_length", "pooler")
     @first_value("max_length", "pooler")
@@ -51,7 +51,6 @@ class _InferFuncWrapper:
         TimeUtils().start(task_name=task_name)
         sequence_batch = sequence
         pooler = pooler.decode("utf-8")
-        # logger.info(f"[_infer_fn_embedding] sequence len: {len(sequence_batch)}; max_length: {max_length}, pooler: {pooler}")
         
         # need to convert dtype=object to bytes first
         # end decode unicode bytes
@@ -60,7 +59,7 @@ class _InferFuncWrapper:
         # import itertools
         # sequence_batch = list(itertools.chain(*sequence_batch))
         sequence_batch = [s[0] for s in sequence_batch]
-        logger.info(f"[_infer_fn_embedding] sequence_batch: {sequence_batch}")
+        logger.info(f"[_infer_fn_embedding] sequence len: {len(sequence_batch)}; max_length: {max_length}, pooler: {pooler}， sequence_batch: {sequence_batch}")
 
         inputs = self._tokenizer(
             sequence_batch, 
@@ -104,6 +103,68 @@ class _InferFuncWrapper:
         TimeUtils().print(task_name=task_name)
         return {"embedding": embeddings}
         # ================================================================================================
+    
+    # # 返回的embedding转换成bytes
+    # @batch
+    # @group_by_values("max_length", "pooler")
+    # @first_value("max_length", "pooler")
+    # def __call__(self, sequence: np.ndarray, max_length: np.int32, pooler: np.bytes_):
+    #     task_name = f"embedding_{uuid.uuid1()}"
+    #     TimeUtils().start(task_name=task_name)
+    #     sequence_batch = sequence
+    #     pooler = pooler.decode("utf-8")
+        
+    #     # need to convert dtype=object to bytes first
+    #     # end decode unicode bytes
+    #     sequence_batch = np.char.decode(sequence_batch.astype("bytes"), "utf-8")
+    #     # logger.info(f"[_infer_fn_embedding] sequence_batch: {sequence_batch}")
+    #     # import itertools
+    #     # sequence_batch = list(itertools.chain(*sequence_batch))
+    #     sequence_batch = [s[0] for s in sequence_batch]
+    #     logger.info(f"[_infer_fn_embedding] sequence len: {len(sequence_batch)}; max_length: {max_length}, pooler: {pooler}， sequence_batch: {sequence_batch}")
+
+    #     inputs = self._tokenizer(
+    #         sequence_batch, 
+    #         padding=True,
+    #         truncation=True,
+    #         max_length=max_length,
+    #         return_tensors="pt"
+    #     )
+    #     inputs_on_device = {k: v.to(device) for k, v in inputs.items()}
+    #     TimeUtils().append("前处理", task_name=task_name)
+    #     outputs = self._model(**inputs_on_device, return_dict=True)
+    #     TimeUtils().append("推理", task_name=task_name)
+    #     # logger.info(f"[_infer_fn_embedding] outputs: {outputs}")
+
+    #     # ================================================================================================
+    #     # last_hidden_states = outputs.last_hidden_state.unsqueeze(1).cpu().detach().numpy()
+    #     # last_hidden_states = np.array(last_hidden_states, dtype=np.float32)
+    #     # logger.info(f"[_infer_fn_embedding] last_hidden_states shape: {last_hidden_states.shape}")
+    #     # return {"last_hidden_state": embeddings}
+    #     # ================================================================================================
+    #     if pooler == "cls":
+    #         embeddings = outputs.last_hidden_state[:, 0]
+    #     elif pooler == "mean":
+    #         attention_mask = inputs_on_device['attention_mask']
+    #         last_hidden = outputs.last_hidden_state
+    #         embeddings = (last_hidden * attention_mask.unsqueeze(-1).float()).sum(1) / attention_mask.sum(-1).unsqueeze(-1)
+    #     else:
+    #         raise NotImplementedError
+                
+    #     # Normalize embedddings
+    #     embeddings = embeddings / embeddings.norm(dim=1, keepdim=True)
+    #     # embeddings_collection.append(embeddings.cpu())
+    #     # embeddings = torch.cat(embeddings_collection, dim=0)
+    #     # embeddings = embeddings.detach().numpy()
+    #     embeddings = embeddings.cpu().detach().numpy().tobytes()
+    #     embeddings = np.array([[embeddings]], dtype=np.bytes_)
+    #     # logger.info(f"[_infer_fn_embedding] bytes_: len: {len(embeddings[0][0])}, {type({embeddings[0][0]})}, {embeddings}")
+    #     # ========================================================================================================================
+
+    #     TimeUtils().append("后处理", task_name=task_name)
+    #     TimeUtils().print(task_name=task_name)
+    #     return {"embedding": embeddings}
+    #     # ================================================================================================
 
 
     # # 返回的embedding转换成bytes
